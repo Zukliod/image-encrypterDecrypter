2378da3b598111fb7499ba19b28bdc274cc699ea
Zukliod <156461915+Zukliod@users.noreply.github.com>
2025-04-23 15:05:07 +0530

    REVERSE CHANGES

--- decryption.py
+++ decryption.py
@@ -1,26 +1,37 @@
-from cryptography.fernet import Fernet
-
+# try block to handle the exception
 try:
-    # Load the encryption key
-    with open('encryption_key.key', 'rb') as key_file:
-        key = key_file.read()
-    cipher = Fernet(key)
-
-    # Take the path of the encrypted image as input
-    path = input(r'Enter path of Encrypted Image: ')
-
-    # Open the encrypted image file in binary read mode
-    with open(path, 'rb') as file:
-        encrypted_data = file.read()
-
-    # Decrypt the image data
-    decrypted_data = cipher.decrypt(encrypted_data)
-
-    # Write the decrypted data back to the file
-    with open(path, 'wb') as file:
-        file.write(decrypted_data)
-
-    print('Decryption Done.')
-
+    # take path of image as a input
+    path = input(r'Enter path of Image : ')
+     
+    # taking decryption key as input
+    key = int(input('Enter Key for encryption of Image : '))
+     
+    # print path of image file and decryption key that we are using
+    print('The path of file : ', path)
+    print('Note : Encryption key and Decryption key must be same.')
+    print('Key for Decryption : ', key)
+     
+    # open file for reading purpose
+    fin = open(path, 'rb')
+     
+    # storing image data in variable "image"
+    image = fin.read()
+    fin.close()
+     
+    # converting image into byte array to perform decryption easily on numeric data
+    image = bytearray(image)
+ 
+    # performing XOR operation on each value of bytearray
+    for index, values in enumerate(image):
+        image[index] = values ^ key
+ 
+    # opening file for writing purpose
+    fin = open(path, 'wb')
+     
+    # writing decryption data in image
+    fin.write(image)
+    fin.close()
+    print('Decryption Done...')
+ 
 except Exception as e:
     print('Error caught:', e)
