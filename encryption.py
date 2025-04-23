9de589fd27a7a3b65c2eba85f50b384aec8a3cd0
Zukliod <156461915+Zukliod@users.noreply.github.com>
2025-04-23 11:22:24 +0530

    NEW CHANGES

--- encryption.py
+++ encryption.py
@@ -1,38 +1,29 @@
-# try block to handle exception
+from cryptography.fernet import Fernet
+
 try:
-    # take path of image as a input
-    path = input(r'Enter path of Image : ')
-     
-    # taking encryption key as input
-    key = int(input('Enter Key for encryption of Image : '))
-     
-    # print path of image file and encryption key that
-    # we are using
-    print('The path of file : ', path)
-    print('Key for encryption : ', key)
-     
-    # open file for reading purpose
-    fin = open(path, 'rb')
-     
-    # storing image data in variable "image"
-    image = fin.read()
-    fin.close()
-     
-    # converting image into byte array to 
-    # perform encryption easily on numeric data
-    image = bytearray(image)
- 
-    # performing XOR operation on each value of bytearray
-    for index, values in enumerate(image):
-        image[index] = values ^ key
- 
-    # opening file for writing purpose
-    fin = open(path, 'wb')
-     
-    # writing encrypted data in image
-    fin.write(image)
-    fin.close()
-    print('Encryption Done...')
- 
+    # Generate or load a key
+    key = Fernet.generate_key()
+    cipher = Fernet(key)
+
+    # Save the key to a file for later decryption
+    with open('encryption_key.key', 'wb') as key_file:
+        key_file.write(key)
+
+    # Take the path of the image as input
+    path = input(r'Enter path of Image: ')
+
+    # Open the image file in binary read mode
+    with open(path, 'rb') as file:
+        image_data = file.read()
+
+    # Encrypt the image data
+    encrypted_data = cipher.encrypt(image_data)
+
+    # Write the encrypted data back to the file
+    with open(path, 'wb') as file:
+        file.write(encrypted_data)
+
+    print('Encryption Done. Key saved to "encryption_key.key".')
+
 except Exception as e:
     print('Error caught:', e)
